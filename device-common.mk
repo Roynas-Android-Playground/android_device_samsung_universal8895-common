fb31a878df7294823204c14d58c19dd7e2bc3ef6
Robert Shih <robertshih@google.com>
2023-03-08 14:45:39 +0900

    universal8895: Enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c

--- device-common.mk
+++ device-common.mk
@@ -97,12 +97,10 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0 \
-    android.hardware.drm@1.1 \
     libfwdlockengine \
     libdrmclearkeyplugin \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.0-impl
 
 # Fingerprint
