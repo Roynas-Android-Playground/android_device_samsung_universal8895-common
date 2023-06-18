8981f062af3c4633c121763ef587458179508830
roynatech2544 <whiteshell2544@naver.com>
2023-06-18 11:14:36 +0900

    universal8895: Switch to sensor impl from hardware/samsung

--- device-common.mk
+++ device-common.mk
@@ -280,8 +280,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0 \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl.samsung \
     android.hardware.sensors@1.0-service
 
 # Samsung Ext
